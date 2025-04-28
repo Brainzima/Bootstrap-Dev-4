d3e23d8b8926259a843f2342d7619e13577910e3
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-04-28 12:10:46 +0000

    color swatcher & Picker

---
+++ 09_BS-JS-ColorPicker/script.js
@@ -0,0 +1,6 @@
+function colorPicker() {
+    const incode = document.getElementById('incode').value 
+    document.getElementById('codeValue').innerText=incode
+    document.getElementById('box').style.backgroundColor=incode
+
+}
